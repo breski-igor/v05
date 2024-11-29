246ebdb29ee266f82f66e42d31ed1244dd176706
breski-igor <igor.breski@vsite.hr>
2024-11-29 06:31:11 +0100

    igor_breskiv_05

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,27 @@
 
 namespace vsite::oop::v5
 {
+	double operator"" _inch(unsigned long long i) {
+		return i * 2.54;
+	}
 
+	double operator"" _eur(unsigned long long e) {
+		return e * 7.5345;
+	}
+
+	double tire::diameter() {
+		return d;
+	}
+
+	int vehicle :: year() {
+		return y;
+	}
+
+	double product::price() {
+		return eur;
+	}
+
+	double car::tire_diameter() {
+		return t.diameter();
+	}
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,40 @@
 #include <string>
 #include <memory>
 
+#define APP_H
+
+
 namespace vsite::oop::v5 
 {
+	double operator"" _inch(unsigned long long i);
+	double operator"" _eur(unsigned long long e);
 
+	class tire {
+		double d;
+	public:
+		tire(double r) : d(r) {};
+		double diameter();
+	};
+
+	class vehicle {
+		int y;
+	public:
+		vehicle(int n) : y(n) {};
+		int year();
+	};
+
+	class product {
+		double eur;
+	public:
+		product(double eur) : eur(eur) {};
+		double price();
+	};
+
+	class car : public vehicle, public product {
+		tire t;
+	public:
+		car(int y, double eur, double d) : vehicle(y), product(eur), t(d) {};
+		double tire_diameter();
+
+	};
 }
